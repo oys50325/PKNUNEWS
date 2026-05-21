06cc54eb629262a826e95c958b1e17fe8a8998c0
oys50325 <oys50325@gmail.com>
2026-05-21 12:45:26 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./styles.css";
+import styles from "./styles.css?inline";
+
+const styleElement = document.createElement("style");
+styleElement.setAttribute("data-ps1-news-styles", "true");
+styleElement.textContent = styles;
+document.head.appendChild(styleElement);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
